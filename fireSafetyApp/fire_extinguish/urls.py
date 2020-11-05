51dc5aa5650c8c3986cf4935e14b7145d870f1cd
abramoconnor <sora.oc@gmail.com>
2020-11-05 00:41:44 -0600

    pdf initial

--- fireSafetyApp/fire_extinguish/urls.py
+++ fireSafetyApp/fire_extinguish/urls.py
@@ -1,7 +1,13 @@
 from rest_framework import routers
+from django.urls import path
 from .api import FEViewSet
+from .views import fe_inspection_render_pdf_view
 
 router = routers.DefaultRouter()
 router.register('fire_extinguish', FEViewSet, 'fire_extinguish')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('fe_insp_pdf/<pk>', fe_inspection_render_pdf_view, name='fe_insp_pdf'),
+]
+
+urlpatterns += router.urls
--- fireSafetyApp/fire_extinguish/views.py
+++ fireSafetyApp/fire_extinguish/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from .models import FireExtinguisher
 
-# Create your views here.
+
+def fe_inspection_render_pdf_view(request, *args, **kwargs):
+    pk = kwargs.get('pk')
+    fe = get_object_or_404(FireExtinguisher, pk=pk)
+
+    template_path = 'test.html'
+    context = {'fe': fe}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    # if download:
+    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # if display:
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
