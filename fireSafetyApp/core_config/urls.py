5506f51df453db4fca004b1f493ba979e016c401
abramoconnor <sora.oc@gmail.com>
2020-11-05 23:16:50 -0600

    quickie

--- fireSafetyApp/core_config/urls.py
+++ fireSafetyApp/core_config/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('alarms.urls')),
     path('', include('frontend.urls')),
     path('', include('buildings.urls')),
     path('', include('fire_extinguish.urls')),
     path('', include('AEDs.urls')),
     path('', include('sprinklers.urls')),
-    # path('', include('alarms.urls')),
+    path('', include('alarms.urls')),
     path('', include('accounts.urls'))
 ]
